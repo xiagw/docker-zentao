cf9cfa92949bf3bcdee2c32dfef4d8d5cd5fd591
xiagw <fxiaxiaoyu@gmail.com>
2021-04-14 13:11:29 +0800

    update docker-entrypoint.sh

--- zentao/docker-entrypoint.sh
+++ zentao/docker-entrypoint.sh
@@ -2,19 +2,14 @@
 
 [ "$DEBUG" ] && set -x
 
-# if [ "$(ls -A /app/zentaopms)" = "" ]; then
-#   cp -a /var/www/zentaopms/* /app/zentaopms
-# fi
 if [ ! -f /app/zentaopms/VERSION ] || [ "$(cat /app/zentaopms/VERSION)" != "$(cat /var/www/zentaopms/VERSION)" ]; then
   cp -a /var/www/zentaopms/* /app/zentaopms
 fi
 chmod -R 777 /app/zentaopms/www/data /app/zentaopms/tmp
 chmod 777 /app/zentaopms/www /app/zentaopms/config
 chmod -R a+rx /app/zentaopms/bin/*
-
-/etc/init.d/apache2 start
-
 chown -R www-data:www-data /app/zentaopms
 
+# /etc/init.d/apache2 start
 # tail -f /var/log/apache2/zentao_error_log
 apache2ctl -D FOREGROUND
